cc7c2546270180a5715c9bd270fefe34d39442d9
Adam Kewley <contact@adamkewley.com>
2019-08-26 16:44:57 +0100

    Switched to statically compiling libdeflate (most CI systems don't package it yet in (e.g.) apt)

--- build.rs
+++ build.rs
@@ -1,13 +1,28 @@
 extern crate bindgen;
+extern crate cc;
 
 use std::env;
 use std::path::PathBuf;
 
 fn main() {
-    println!("cargo:rustc-link-lib=deflate");
+    cc::Build::new()
+        .file("libdeflate/lib/aligned_malloc.c")
+        .file("libdeflate/lib/deflate_decompress.c")
+        .file("libdeflate/lib/arm/cpu_features.c")
+        .file("libdeflate/lib/x86/cpu_features.c")
+        .file("libdeflate/lib/deflate_compress.c")
+        .file("libdeflate/lib/adler32.c")
+        .file("libdeflate/lib/zlib_decompress.c")
+        .file("libdeflate/lib/zlib_compress.c")
+        .file("libdeflate/lib/crc32.c")
+        .file("libdeflate/lib/gzip_decompress.c")
+        .file("libdeflate/lib/gzip_compress.c")
+        .include("libdeflate/common/")
+        .warnings(false)
+        .compile("libdeflate");
 
     let bindings = bindgen::Builder::default()
-        .header("src/wrapper.h")
+        .header("libdeflate/libdeflate.h")
         .generate()
         .expect("Unable to generate bindings");
 
