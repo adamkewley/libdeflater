926f6b54f6251b11782a782de7fc7cc41730883a
Adam Kewley <contact@adamkewley.com>
2019-09-02 11:27:01 +0100

    Added 'decompress_gz' method /w basic tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,77 @@
 mod libdeflate_sys;
+
+#[allow(non_upper_case_globals)]
+pub mod inflate {
+    use crate::libdeflate_sys::{libdeflate_decompressor,
+                                libdeflate_alloc_decompressor,
+                                libdeflate_free_decompressor,
+                                libdeflate_gzip_decompress,
+                                libdeflate_result,
+                                libdeflate_result_LIBDEFLATE_SUCCESS,
+                                libdeflate_result_LIBDEFLATE_BAD_DATA,
+                                libdeflate_result_LIBDEFLATE_INSUFFICIENT_SPACE};
+    
+    pub struct Decompressor {
+        p: *mut libdeflate_decompressor,
+    }
+
+    #[derive(Debug, PartialEq)]
+    pub enum Error {
+        BadData,
+        InsufficientSpace,
+    }
+
+    type Result<T> = std::result::Result<T, Error>;
+
+    impl Decompressor {
+        pub fn new() -> Decompressor {
+            unsafe {
+                let ptr = libdeflate_alloc_decompressor();
+                if !ptr.is_null() {
+                    Decompressor{ p: ptr }
+                } else {
+                    panic!("libdeflate_alloc_decompressor returned NULL: out of memory");
+                }            
+            }
+        }
+
+        pub fn decompress_gz(&mut self,
+                             deflate_data: &[u8],
+                             out: &mut [u8]) -> Result<usize> {
+            unsafe {
+                let mut out_nbytes = 0;
+                let in_ptr = deflate_data.as_ptr() as *const std::ffi::c_void;
+                let out_ptr = out.as_mut_ptr() as *mut std::ffi::c_void;
+                let ret: libdeflate_result =
+                    libdeflate_gzip_decompress(self.p,
+                                               in_ptr,
+                                               deflate_data.len(),
+                                               out_ptr,
+                                               out.len(),
+                                               &mut out_nbytes);
+                match ret {
+                    libdeflate_result_LIBDEFLATE_SUCCESS => {
+                        Ok(out_nbytes)
+                    },
+                    libdeflate_result_LIBDEFLATE_BAD_DATA => {
+                        Err(Error::BadData)
+                    },
+                    libdeflate_result_LIBDEFLATE_INSUFFICIENT_SPACE => {
+                        Err(Error::InsufficientSpace)
+                    },
+                    _ => {
+                        panic!("libdeflate_gzip_decompress returned an unknown error type: this is an internal bug that **must** be fixed");
+                    }
+                }
+            }
+        }
+    }
+
+    impl Drop for Decompressor {
+        fn drop(&mut self) {
+            unsafe {
+                libdeflate_free_decompressor(self.p);
+            }
+        }
+    }
+}
---
+++ tests/integration_test.rs
@@ -0,0 +1,146 @@
+extern crate libdeflate;
+
+use std::fs::File;
+use std::io::Read;
+use std::vec::Vec;
+use libdeflate::inflate::{Decompressor, Error};
+
+#[test]
+fn test_can_create_new_decompressor() {
+    Decompressor::new();
+}
+
+fn read_fixture(pth: &str) -> Vec<u8> {
+    let mut f = File::open(pth).unwrap();
+    let mut content = Vec::new();
+    f.read_to_end(&mut content).unwrap();
+    content
+}
+
+fn read_fixture_content() -> Vec<u8> {
+    read_fixture("tests/hello")
+}
+
+fn fixture_content_size() -> usize {
+    read_fixture_content().len()
+}
+
+fn read_fixture_gz() -> Vec<u8> {
+    read_fixture("tests/hello.gz")
+}
+
+fn read_fixture_gz_with_bad_magic_num() -> Vec<u8> {
+    let mut data = read_fixture_gz();
+    
+    // beats having to have an extra fixture file
+    data[0] = 0x00;
+
+    data
+}
+
+fn read_fixture_gz_with_bad_crc32() -> Vec<u8> {
+    let mut data = read_fixture_gz();
+    let crc32_start = data.len()-8;
+    
+    // beats having to have an extra fixture file
+    data[crc32_start] = data[crc32_start] + 1;
+
+    data
+}
+
+fn read_fixture_gz_with_bad_isize() -> Vec<u8> {
+    let mut data = read_fixture_gz();
+    let isize_start = data.len()-4;
+    
+    // beats having to have an extra fixture file
+    data[isize_start] = data[isize_start] + 1;
+
+    data
+}
+
+#[test]
+fn test_can_call_decompress_gz_with_valid_args() {
+    let mut decompressor = Decompressor::new();
+    let content = read_fixture_gz();
+    let mut decompressed = Vec::new();
+    decompressed.resize(fixture_content_size(), 0);
+
+    decompressor.decompress_gz(&content, &mut decompressed).unwrap();
+}
+
+#[test]
+fn test_calling_decompress_gz_with_undersized_outbuf_returns_insufficient_space() {
+    let result = {
+        let mut decompressor = Decompressor::new();
+        let content = read_fixture_gz();
+        let mut decompressed = Vec::new();
+        decompressor.decompress_gz(&content, &mut decompressed)
+    };
+    
+    assert_eq!(result.unwrap_err(), Error::InsufficientSpace);
+}
+
+#[test]
+fn test_calling_decompress_gz_with_valid_buf_fills_buf_with_expected_content() {
+    let decompressed = {
+        let mut decompressor = Decompressor::new();
+        let content = read_fixture_gz();
+        let mut decompressed = Vec::new();
+        decompressed.resize(fixture_content_size(), 0);
+        decompressor.decompress_gz(&content, &mut decompressed).unwrap();
+        decompressed
+    };
+
+    let expected_content = read_fixture_content();
+
+    assert_eq!(decompressed, expected_content);
+}
+
+#[test]
+fn test_calling_decompress_gz_with_oversized_buf_returns_correct_size() {
+    const OVERSIZED_FACTOR: usize = 2;
+    
+    let sz = {
+        let mut decompressor = Decompressor::new();
+        let content = read_fixture_gz();
+        let mut decompressed = Vec::new();
+        decompressed.resize(OVERSIZED_FACTOR*fixture_content_size(), 0);
+        let sz = decompressor.decompress_gz(&content, &mut decompressed).unwrap();
+        sz
+    };
+
+    assert_eq!(sz, fixture_content_size());
+}
+
+#[test]
+fn test_calling_decompress_gz_with_bad_magic_num_returns_bad_data() {
+    let mut decompressor = Decompressor::new();
+    let content = read_fixture_gz_with_bad_magic_num();
+    let mut decompressed = Vec::new();
+    decompressed.resize(fixture_content_size(), 0);
+    let result = decompressor.decompress_gz(&content, &mut decompressed);
+
+    assert_eq!(result.unwrap_err(), Error::BadData);
+}
+
+#[test]
+fn test_calling_decompress_gz_with_corrupted_crc32_returns_bad_data() {
+    let mut decompressor = Decompressor::new();
+    let content = read_fixture_gz_with_bad_crc32();
+    let mut decompressed = Vec::new();
+    decompressed.resize(fixture_content_size(), 0);
+    let result = decompressor.decompress_gz(&content, &mut decompressed);
+
+    assert_eq!(result.unwrap_err(), Error::BadData);
+}
+
+#[test]
+fn test_calling_decompress_gz_with_corrupted_isize_returns_bad_data() {
+    let mut decompressor = Decompressor::new();
+    let content = read_fixture_gz_with_bad_isize();
+    let mut decompressed = Vec::new();
+    decompressed.resize(fixture_content_size(), 0);
+    let result = decompressor.decompress_gz(&content, &mut decompressed);
+
+    assert_eq!(result.unwrap_err(), Error::BadData);
+}
