44bbc45155c9b3fee5286c718124d8e88ea32fe2
Adam Kewley <contact@adamkewley.com>
2019-09-29 10:17:18 +0100

    Removed bindgen dependency
    - Was introducing a 'libdeflate' 3rd-party dep for downstream devs
    - Now downstream devs only need a valid C compiler installed (which is
      *much* more common)

--- build.rs
+++ build.rs
@@ -1,8 +1,4 @@
-extern crate bindgen;
 extern crate cc;
-
-use std::env;
-use std::path::PathBuf;
 
 fn main() {
     cc::Build::new()
@@ -22,14 +18,4 @@ fn main() {
         .include("libdeflate/common/")
         .warnings(false)
         .compile("libdeflate");
-
-    let bindings = bindgen::Builder::default()
-        .header("libdeflate/libdeflate.h")
-        .generate()
-        .expect("Unable to generate bindings");
-
-    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
-    bindings
-        .write_to_file(out_path.join("bindings.rs"))
-        .expect("Couldn't write bindings!");
 }
