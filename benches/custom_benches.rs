d0251a3115d6c99377a065bbffd678c1ccebea14
Adam Kewley <contact@adamkewley.com>
2019-09-30 20:06:39 +0100

    Optimized flate2 and libdeflate in benchmark
    - Both now use a very similar API
    - libdeflate optimized to use the same unsafe {} methods flate2 uses
    - benchmark results updated

--- benches/custom_benches.rs
+++ benches/custom_benches.rs
@@ -4,7 +4,6 @@ extern crate toml;
 
 use std::io::prelude::*;
 use std::fs;
-use std::io;
 use std::fs::{File};
 use std::collections::{HashMap};
 use serde::{Deserialize};
@@ -13,9 +12,7 @@ use serde::{Deserialize};
 use criterion::{Criterion};
 
 // (de)compression libs
-use flate2::Compression;
-use flate2::write::GzEncoder;
-use flate2::read::GzDecoder;
+use flate2::{Compression, Compress, Decompress, FlushCompress, FlushDecompress};
 use libdeflater::{Compressor, CompressionLvl, Decompressor};
 
 
@@ -29,30 +26,33 @@ struct CustomBenchmark {
 
 
 // flate2
-struct Flate2Encoder();
+struct Flate2Encoder {
+    compress: Compress,
+}
 
 impl Flate2Encoder {
     fn new() -> Flate2Encoder {
-        Flate2Encoder{}
+        Flate2Encoder{ compress: Compress::new(Compression::default(), true) }
     }
 
     fn encode(&mut self, data: &[u8], out: &mut Vec<u8>) {
-        let mut e = GzEncoder::new(out, Compression::default());
-        e.write_all(data).unwrap();
-        e.finish().unwrap();
+        self.compress.compress_vec(data, out, FlushCompress::Finish).unwrap();
+        self.compress.reset();
     }
 }
 
-struct Flate2Decoder();
+struct Flate2Decoder {
+    decompress: Decompress,
+}
 
 impl Flate2Decoder {
     fn new() -> Flate2Decoder {
-        Flate2Decoder{}
+        Flate2Decoder { decompress: Decompress::new(true) }
     }
 
-    fn decode(&mut self, gz_data: &[u8], _decomp_sz: usize, out: &mut Vec<u8>) {
-        let mut d = GzDecoder::new(gz_data);
-        io::copy(&mut d, out).unwrap();
+    fn decode(&mut self, compressed_data: &[u8], _decomp_sz: usize, out: &mut Vec<u8>) {
+        self.decompress.decompress_vec(compressed_data, out, FlushDecompress::Finish).unwrap();
+        self.decompress.reset(true);
     }
 }
 
@@ -69,9 +69,15 @@ impl LibdeflateEncoder {
     }
 
     fn encode(&mut self, data: &[u8], out: &mut Vec<u8>) {
-        out.resize(self.compressor.compress_gzip_bound(data.len()), 0);
-        let actual = self.compressor.compress_gzip(data, out).unwrap();
-        out.resize(actual, 0);
+        // This unsafe is fair for this particular comparison with
+        // flate2's high-performance `Compress::compress_vec` method
+        // because it essentially does exactly the same thing under
+        // the hood
+        unsafe {
+            out.set_len(self.compressor.compress_zlib_bound(data.len()));
+            let actual = self.compressor.compress_zlib(data, out).unwrap();
+            out.set_len(actual);
+        }
     }
 }
 
@@ -85,10 +91,16 @@ impl LibdeflateDecoder {
     }
 
 
-    fn decode(&mut self, gz_data: &[u8], decomp_sz: usize, out: &mut Vec<u8>) {
-        out.resize(decomp_sz, 0);
-        let sz = self.st.decompress_gzip(gz_data, out).unwrap();
-        out.resize(sz, 0);
+    fn decode(&mut self, zlib_data: &[u8], decomp_sz: usize, out: &mut Vec<u8>) {
+        // This unsafe is fair for this particular comparison with
+        // flate2's high-performance `Decompresss::decompress_vec`
+        // method because it essentially does exactly the same thing
+        // under the hood
+        unsafe {
+            out.set_len(decomp_sz);
+            let sz = self.st.decompress_zlib(zlib_data, out).unwrap();
+            out.set_len(sz);
+        }
     }
 }
 
@@ -98,10 +110,16 @@ impl LibdeflateDecoder {
 // want to benchmark different corpuses of data to see if libdeflate
 // is suitable for their needs
 pub fn run_custom_benches(b: &mut Criterion) {
+    // flate2's low-level API will only handle the vector length, not
+    // capacity, and flate2 doesn't have a "bounds" method so that
+    // callers can pre-size the buffer for it, so this hack just makes
+    // sure we have a buffer big enough to accomodate the corpus.
+    const BUF_CAP_BIG_ENOUGH_TO_FIT_DATA: usize = 1<<20;
+
     let cfg: HashMap<String, CustomBenchmark> =
         toml::from_str(&fs::read_to_string("benches/custom-benches.toml").unwrap()).unwrap();
 
-    let mut buf: Vec<u8> = Vec::new();
+    let mut buf: Vec<u8> = Vec::with_capacity(BUF_CAP_BIG_ENOUGH_TO_FIT_DATA);
     let mut flate2_encoder = Flate2Encoder::new();
     let mut libdeflate_encoder = LibdeflateEncoder::new();
     let mut flate2_decoder = Flate2Decoder::new();
@@ -122,17 +140,17 @@ pub fn run_custom_benches(b: &mut Criterion) {
         };
 
         grp.bench_function("flate2_encode", |b| b.iter(|| {
+            buf.clear();
             flate2_encoder.encode(&raw_data, &mut buf);
-            buf.resize(0, 0);
         }));
 
         grp.bench_function("libdeflate_encode", |b| b.iter(|| {
+            buf.clear();
             libdeflate_encoder.encode(&raw_data, &mut buf);
-            buf.resize(0, 0);
         }));
 
         let compressed_data = {
-            let mut buf = Vec::new();
+            let mut buf = Vec::with_capacity(BUF_CAP_BIG_ENOUGH_TO_FIT_DATA);
             Flate2Encoder::new().encode(&raw_data, &mut buf);
             buf
         };
